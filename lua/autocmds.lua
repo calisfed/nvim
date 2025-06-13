8d0ac9d774dcf3714cb8c369f2e02e4dabbe13e4
calisfed <cstory1996@gmail.com>
2025-06-13 19:52:22 +0700

    add restore last place autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -526,3 +526,23 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
 -- 	  vim.api.nvim_buf_set_lines(0, start_line, end_line, false, new_lines)
 
 -- end
+
+vim.api.nvim_create_autocmd('BufRead', {
+  callback = function(opts)
+    vim.api.nvim_create_autocmd('BufWinEnter', {
+      once = true,
+      buffer = opts.buf,
+      callback = function()
+        local ft = vim.bo[opts.buf].filetype
+        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+        if
+          not (ft:match('commit') and ft:match('rebase'))
+          and last_known_line > 1
+          and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+        then
+          vim.api.nvim_feedkeys([[g`"]], 'nx', false)
+        end
+      end,
+    })
+  end,
+})
