5c14e30d1724e09328256c0b696097d1daf99d49
calisfed <cstory1996@gmail.com>
2025-06-23 22:11:36 +0700

    config oil.nvim

--- lua/plug/oil.lua
+++ lua/plug/oil.lua
@@ -1,38 +1,47 @@
 return {
-    enabled = false,
+  enabled = false,
   'stevearc/oil.nvim',
   lazy = false,
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- keys = { '-', '<cmd>Oil<CR>' , desc = "File explorer"},
-  opts = {
-    vim.keymap.set('n', '-', '<cmd>lua require"oil".toggle_float()<CR>', { desc = 'File explorer', silent = true }),
-    cleanup_delay_ms = 100,
-    skip_confirm_for_simple_edits = true,
-    prompt_save_on_select_new_entry = false,
-    delete_to_trash = true,
-    colums = { "icons", "permissions", "size", "mtime", },
-    win_options = {
-      signcolumn = "no",
-    },
-    view_options = { show_hidden = true, },
+  config = function()
+    require('oil').setup({
+      vim.keymap.set('n', '-', '<cmd>lua require"oil".toggle_float()<CR>', { desc = 'File explorer', silent = true }),
+      -- vim.keymap.set('n', '-', '<cmd>lua require"oil".open()<CR>', { desc = 'File explorer', silent = true }),
 
-    keymaps = {
-      ['<C-space>'] = 'actions.select',
-      ['h'] = 'actions.parent',
-      ['l'] = 'actions.select',
-      ['<Down>'] = 'actions.preview_scroll_down',
-      ['<Up>'] = 'actions.preview_scroll_up',
-      ['<Esc>'] = 'actions.close',
-    },
-
-    float = {
-      padding = 2,
-      max_width = 150,
-      max_height = 200,
-      border = 'rounded',
+      cleanup_delay_ms = 100,
+      skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = false,
+      delete_to_trash = true,
+      colums = { "icons", "permissions", "size", "mtime", },
       win_options = {
-        winblend = 0,
+        signcolumn = "yes",
       },
-    },
-  },
+      view_options = { show_hidden = true, },
+      watch_for_changes = true,
+
+      keymaps = {
+        ['<C-space>'] = 'actions.select',
+        -- ['h'] = 'actions.parent',
+        ['<C-h>'] = 'actions.parent',
+        ['l'] = 'actions.select',
+        ['<Down>'] = 'actions.preview_scroll_down',
+        ['<Up>'] = 'actions.preview_scroll_up',
+        ['<Esc>'] = 'actions.close',
+      },
+
+      float = {
+        padding = 2,
+        max_width = 150,
+        max_height = 200,
+        border = 'rounded',
+        win_options = {
+          winblend = 0,
+        },
+      },
+    }
+
+    )
+  end
+
 }
