b278bb580d55fd55c5bf96aa65e999b237309716
calisfed <cstory1996@gmail.com>
2025-06-26 11:09:29 +0700

    Change: concealcursor, asciidoc-preview
    Add concealcursor = nic
    Make Asciidoc-preview plugin work locally

--- lua/plug/asciidoc-preview.lua
+++ lua/plug/asciidoc-preview.lua
@@ -6,10 +6,11 @@ return {
 	keys = {
 		{"<leader>na", "<cmd>AsciiDocPreview<cr>", desc = "Preview Asciidoc"},
 	},
-	build = 'cd server && npm install --omit=dev',
+	-- build = 'cd server && npm install --omit=dev',
 	opts = {
 		server = {
-			converter = 'js',
+			-- converter = 'js', -- online, no need asciidoctor
+			converter = 'cmd', -- local, need asciidoctor
 			port = 11235,
 		},
 		preview = {
