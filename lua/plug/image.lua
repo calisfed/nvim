85d7f6d4db0f6cedc07202a5cbe5ffcdaed40c16
calisfed <cstory1996@gmail.com>
2026-03-30 12:14:43 -0700

    disable image.nvim when using neovide

--- lua/plug/image.lua
+++ lua/plug/image.lua
@@ -6,10 +6,13 @@ return
   enabled = false,
   -- event = 'VeryLazy',
   dependencies = { { "vhyrro/luarocks.nvim", priority = 1000, config = true, } },
+  cond = function()
+    return not vim.g.neovide
+  end,
   config = function()
     -- default config
     require("image").setup({
-      backend = "ueberzug", --ueberzug or kitty
+      backend = "ueberzug",   --ueberzug or kitty
       processor = "magick_cli",
       integrations = {
         markdown = {
@@ -17,7 +20,7 @@ return
           clear_in_insert_mode = true,
           download_remote_images = false,
           only_render_image_at_cursor = true,
-          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+          filetypes = { "markdown", "vimwiki" },   -- markdown extensions (ie. quarto) can go here
         },
         typst = {
           enabled = false,
@@ -44,12 +47,12 @@ return
       max_height = nil,
       max_width_window_percentage = nil,
       max_height_window_percentage = 50,
-      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_enabled = false,   -- toggles images when windows are overlapped
       -- window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
       -- window_overlap_clear_ft_ignore = { "noice", "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-      editor_only_render_when_focused = false,                                            -- auto show/hide images when the editor gains/looses focus
-      tmux_show_only_in_active_window = true,                                             -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+      editor_only_render_when_focused = false,                                              -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = true,                                               -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },   -- render image files as images when opened
     })
   end
 }
